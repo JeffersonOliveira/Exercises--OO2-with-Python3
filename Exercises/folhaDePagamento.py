a81ff36530e3d368e9ba105f965a3ff9bad26b6b
Jefferson Nunes S. de Oliveira <jefferson_nunes01@hotmail.com>
2021-11-02 00:18:35 -0300

    Adding some exercises about @staticmethods and @classmethods

---
+++ Exercises/folhaDePagamento.py
@@ -0,0 +1,10 @@
+class FolhaDePagamento:
+
+    @staticmethod
+    def log():
+        return f'Isso é um log qualquer.'
+
+#folha = FolhaDePagamento()
+#print(folha.log())
+
+print(FolhaDePagamento.log())
