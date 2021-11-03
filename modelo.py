4f5c8b78ff9b5924ca7b2aa37ece4f6511f22050
Jefferson Nunes S. de Oliveira <jefferson_nunes01@hotmail.com>
2021-11-02 22:56:36 -0300

    Implementing __str__ to represent an object as a string

--- modelo.py
+++ modelo.py
@@ -4,8 +4,8 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes} Like(s)')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Like(s)'
 
     @property
     def likes(self):
@@ -27,16 +27,16 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Like(s)')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Like(s)'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Like(s)')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Like(s)'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -51,4 +51,6 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
+
+
